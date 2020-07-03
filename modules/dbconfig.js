f16257285cc47b1dd6d78c712ebf0680fa9dfe3c
kieu-lala <66350043+kieu-lala@users.noreply.github.com>
2020-07-03 22:49:13 +0700

    Add files via upload

---
+++ modules/dbconfig.js
@@ -0,0 +1,21 @@
+/// ***************** ***************** *****************
+/// ***************** ***************** Config DB CONNECTION
+const MongoClient = require('mongodb').MongoClient;
+const mongosee = require('mongoose');
+
+/// ***************** 
+const uri = "mongodb+srv://db03:aGBZRta11CBmt8qL@cluster0-q8a6f.mongodb.net/CloudDB?retryWrites=true&w=majority";
+
+
+/// ***************** ***************** *****************
+/// ***************** Database & Bảng dữ liệu cần Truy vấn
+const NameDataBase =  "atnshop"; // "CloudDB";
+
+
+
+module.exports = {
+    Client : MongoClient,
+    Mongosee : mongosee,
+    URI : uri,
+    DBname : NameDataBase,
+};
